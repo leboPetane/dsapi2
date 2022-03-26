f2f87ec2ea5e165a32b1405116d990b168244d5c
Lebohang Petane <lebopetane@gmail.com>
2022-03-26 22:21:54 +0200

    added cross origin final maybe

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express      = require('express');
 var path         = require('path');
 var cookieParser = require('cookie-parser');
 var logger       = require('morgan');
+var cors         = require('cors');
 
 var indexRouter      = require('./routes/index');
 var usersRouter      = require('./routes/users');
@@ -14,7 +15,7 @@ var lessonRouter     = require('./routes/lessons');
 
 
 var app = express();
-
+app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
